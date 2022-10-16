a1bc101898ccba3f5f886f19c70f0a502acae794
Sim0neQ <115417007+Sim0neQ@users.noreply.github.com>
2022-10-16 22:18:30 +0200

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) },
+  { path: 'search/:searchTerm', loadChildren: () => import('./search/search.module').then(m => m.SearchModule) }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ search/search.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchRoutingModule } from './search-routing.module';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    SearchRoutingModule
+  ]
+})
+export class SearchModule { }
+
+import { Routes } from '@angular/router';
+import { style } from '@angular/animations';
+import { of } from 'rxjs';
+
+export const routes: Routes = [
+  {
+    path: '',
+
+  }
+];
+
+
+
+
+
+
+
+
+
